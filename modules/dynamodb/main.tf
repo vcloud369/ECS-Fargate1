b301875ece5442611fbfb8beb74ccff268266809
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-22 13:23:55 +0530

    Create main.tf

---
+++ modules/dynamodb/main.tf
@@ -0,0 +1,9 @@
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
