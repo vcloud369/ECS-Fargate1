da3f0e3bf8727ff6e117121ba78ad93c3606673a
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-22 14:09:57 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -5,16 +5,39 @@ resource "aws_security_group" "ecs_security_group" {
 }
 
 resource "aws_iam_role" "ecs_task_role" {
+  name               = "ecs-task-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
+        Action    = "sts:AssumeRole"
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
         }
+        Effect    = "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "ecs_task_policy" {
+  name   = "ecs-task-policy"
+  role   = aws_iam_role.ecs_task_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetRepositoryPolicy",
+          "ecr:BatchGetImage",
+          "ecs:UpdateService",
+          "ecs:DescribeTasks",
+          "ecs:RunTask"
+        ]
         Effect   = "Allow"
-        Sid      = ""
+        Resource = "*"
       },
     ]
   })
