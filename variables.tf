c1f285f3d0351e9d072f73bdb63a2250a32f28a0
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-22 13:10:48 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr_1" {
+  description = "CIDR block for Public Subnet 1"
+  type        = string
+}
+
+variable "private_subnet_cidr_1" {
+  description = "CIDR block for Private Subnet 1"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "ecs_cluster_name" {
+  description = "ECS Cluster Name"
+  type        = string
+}
+
+variable "docker_registry_url" {
+  description = "Docker registry URL"
+  type        = string
+}
