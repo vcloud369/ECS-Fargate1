7ff156ffb0fbf2c0653cd65a0c555e9088a5bdba
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-22 14:01:31 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-bucket"
+  bucket = "manikanta-terraform-state-bucket"
+}
+
+resource "aws_s3_bucket_acl" "terraform_state_acl" {
+  bucket = aws_s3_bucket.terraform_state.id
   acl    = "private"
 }
