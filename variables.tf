a337684516d6bac7de28693c57993804f22fdf04
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-23 17:20:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,36 +1,68 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "ap-south-1"
+}
+
+
 variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
+  description = "CIDR block for the VPC"
+  default     = "10.1.0.0/16"
 }
 
-variable "public_subnet_cidr_1" {
-  description = "CIDR block for Public Subnet 1"
-  type        = string
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR block for the first public subnet"
+  default     = "10.1.1.0/24"
 }
 
-variable "private_subnet_cidr_1" {
-  description = "CIDR block for Private Subnet 1"
-  type        = string
+variable "private_subnet_1_cidr" {
+  description = "CIDR block for the first private subnet"
+  default     = "10.1.2.0/24"
 }
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR block for the second public subnet"
+  default     = "10.1.3.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR block for the second private subnet"
+  default     = "10.1.4.0/24"
+}
+
+variable "az_1" {
+  description = "Availability Zone 1"
+  default     = "ap-south-1a"
+}
+
+variable "az_2" {
+  description = "Availability Zone 2"
+  default     = "ap-south-1b"
+}
+
 
 variable "ecs_cluster_name" {
-  description = "ECS Cluster Name"
-  type        = string
+  description = "Name of the ECS Cluster"
+  default     = "usecase3-cluster"
 }
 
-variable "docker_registry_url" {
-  description = "Docker registry URL"
-  type        = string
+variable "ecs_task_family" {
+  description = "ECS Task Definition Family"
+  default     = "usecase3-task-family"
 }
 
-variable "container_cpu" {
-  description = "ECS container CPU"
-  type        = string
-  default     = "256"
+variable "patient_service_image" {
+  description = "Docker image URL for the patient service"
+  default     = "314146309097.dkr.ecr.ap-south-1.amazonaws.com/patient-service:latest"
 }
 
-variable "container_memory" {
-  description = "ECS container memory"
-  type        = string
-  default     = "512"
+variable "appointment_service_image" {
+  description = "Docker image URL for the appointment service"
+  default     = "314146309097.dkr.ecr.ap-south-1.amazonaws.com/appointment-service:latest"
+}
+
+
+variable "alb_name" {
+  description = "The name of the Application Load Balancer"
+  default     = "usecase3-alb"
 }
