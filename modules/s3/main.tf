8d6df3d2d618fb8f7c3ff986e1bd794efa5e11ab
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-23 17:48:01 +0530

    Delete modules/s3 directory

--- modules/s3/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "manikanta-terraform-state-bucket"
-}
-
-resource "aws_s3_bucket_acl" "terraform_state_acl" {
-  bucket = aws_s3_bucket.terraform_state.id
-  acl    = "private"
-}
