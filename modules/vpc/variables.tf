81899797a0b17eeaefe3940b35d53ffa50262323
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-23 17:45:12 +0530

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,39 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR block for the first public subnet"
+  type        = string
+}
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR block for the first private subnet"
+  type        = string
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR block for the second public subnet"
+  type        = string
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR block for the second private subnet"
+  type        = string
+}
+
+variable "az_1" {
+  description = "Availability zone for public subnet 1"
+  type        = string
+}
+
+variable "az_2" {
+  description = "Availability zone for public subnet 2"
+  type        = string
+}
