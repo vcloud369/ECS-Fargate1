f167855f748fe05759dca3aeb1ebe29dd30f7804
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-23 17:39:17 +0530

    Create variables.tf

---
+++ modules/ecr/variables.tf
@@ -0,0 +1,9 @@
+variable "patient_service_repo_name" {
+  description = "The name of the patient service ECR repository"
+  type        = string
+}
+
+variable "appointment_service_repo_name" {
+  description = "The name of the appointment service ECR repository"
+  type        = string
+}
