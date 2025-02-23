b998bf77d6163922a42db2689304dd1666427b6a
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-23 17:48:37 +0530

    Delete modules/dynamodb directory

--- modules/dynamodb/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock"
-  hash_key     = "LockID"
-  billing_mode = "PAY_PER_REQUEST"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
