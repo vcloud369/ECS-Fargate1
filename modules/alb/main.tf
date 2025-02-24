af5fa1dd86dd3daa5e37a0e9346e8d72835c7c41
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-24 11:21:49 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -9,17 +9,37 @@ resource "aws_lb" "app_lb" {
 
 resource "aws_lb_target_group" "patient_tg" {
   name     = "patient-service-tg"
-  port     = 80
+  port     = 3000
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "ip"  # Target type set to IP (Fargate tasks)
+
+  health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
 }
 
 
 resource "aws_lb_target_group" "appointment_tg" {
   name     = "appointment-service-tg"
-  port     = 80
+  port     = 3001
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "ip"  # Target type set to IP (Fargate tasks)
+
+  health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
 }
 
 
@@ -42,14 +62,16 @@ resource "aws_lb_listener" "http_listener" {
 resource "aws_lb_listener_rule" "patient_service_rule" {
   listener_arn = aws_lb_listener.http_listener.arn
   priority     = 1
+
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.patient_tg.arn
   }
+
   condition {
-    path_pattern { 
-      values = ["/patient"]
-    } 
+    path_pattern {
+      values = ["/patients"]
+    }
   }
 }
 
@@ -57,13 +79,15 @@ resource "aws_lb_listener_rule" "patient_service_rule" {
 resource "aws_lb_listener_rule" "appointment_service_rule" {
   listener_arn = aws_lb_listener.http_listener.arn
   priority     = 2
+
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.appointment_tg.arn
   }
+
   condition {
-    path_pattern { 
-      values = ["/appointment"]
+    path_pattern {
+      values = ["/appointments"]
     }
   }
 }
