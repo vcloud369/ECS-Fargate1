4eb6d5180c7076064d84713249183b921a4deb52
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-24 12:23:53 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -69,6 +69,28 @@ resource "aws_subnet" "private_subnet_2" {
   }
 }
 
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "ecs-execution-role"
+  }
+}
+
+
 resource "aws_iam_role" "ecs_task_role" {
   name = "ecs-task-role"
 
