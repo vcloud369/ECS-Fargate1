e0b15b2df2d997e446d4df509500d9be53b2dd31
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-24 11:31:48 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -6,12 +6,12 @@ output "public_subnet_1_id" {
   value = aws_subnet.public_subnet_1.id
 }
 
-output "private_subnet_1_id" {
-  value = aws_subnet.private_subnet_1.id
-}
-
 output "public_subnet_2_id" {
   value = aws_subnet.public_subnet_2.id
+}
+
+output "private_subnet_1_id" {
+  value = aws_subnet.private_subnet_1.id
 }
 
 output "private_subnet_2_id" {
