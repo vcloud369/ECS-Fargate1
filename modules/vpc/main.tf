967dc71e507cdbad8b79ecd3b74a54b4912720d2
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-24 13:31:55 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -138,49 +138,7 @@ resource "aws_iam_policy" "ecs_task_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_policy_attachment" {
-  role       = aws_iam_role.ecs_task_role.name
-  policy_arn = aws_iam_policy.ecs_task_policy.arn
-}
 
-
-# ECS Logging and X-Ray Policy for ECS Task Role
-resource "aws_iam_policy" "ecs_logging_xray_policy" {
-  name        = "ecs_logging_xray_policy"
-  description = "Allows ECS tasks to send logs to CloudWatch and traces to X-Ray"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = "arn:aws:logs:*:*:*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "xray:PutTraceSegments",
-          "xray:PutTelemetryRecords"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-} 
-# Attach Logging and X-Ray Policies to ECS Task Role
-resource "aws_iam_role_policy_attachment" "ecs_execution_attach_logging" {
-  role       = aws_iam_role.ecs_task_role.name
-  policy_arn = aws_iam_policy.ecs_logging_xray_policy.arn
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_execution_attach_xray" {
-  role       = aws_iam_role.ecs_task_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
-}
 
 
 resource "aws_route_table" "public" {
