b9040012ed760b95fbb4e469bb5baeb59759b197
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-03-01 16:57:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket = "manikanta-terraform-state-bucket"  
     key    = "path/to/terraform.tfstate"        
     region = "ap-south-1"                       
-    dynamodb_table = "terraform-state-lock"     
   }
 }
