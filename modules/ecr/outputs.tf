5451b2b8ae4e2a6af428fa6f4dd592cb55b4b858
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-03-02 00:18:49 +0530

    Update outputs.tf

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -1,9 +1,9 @@
-# Access the repository URL for patient_service
-#output "patient_service_repo_url" {
- # value = aws_ecr_repository.patient_service.repository_url
-#}
+# Output the URL of the existing patient-service repository
+output "patient_service_repo_url" {
+  value = data.aws_ecr_repository.patient_service.repository_url
+}
 
-# Access the repository URL for appointment_service
-#output "appointment_service_repo_url" {
- # value = aws_ecr_repository.appointment_service.repository_url
-#}
+# Output the URL of the existing appointment-service repository
+output "appointment_service_repo_url" {
+  value = data.aws_ecr_repository.appointment_service.repository_url
+}
