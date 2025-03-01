86dbf6e5c3b2e10e79eaa6714b5b3c3658f6b510
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-03-02 00:11:20 +0530

    Update main.tf

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,27 +1,7 @@
-# Check if patient-service repository exists
-resource "null_resource" "check_patient_service_repo" {
-  provisioner "local-exec" {
-    command = "aws ecr describe-repositories --repository-names ${var.patient_service_repo_name} || true"
-  }
+data "aws_ecr_repository" "patient_service" {
+  name = var.patient_service_repo_name
 }
 
-# Create patient-service repository if it doesn't exist
-resource "aws_ecr_repository" "patient_service" {
-  depends_on = [null_resource.check_patient_service_repo]
-  name       = var.patient_service_repo_name
-  image_tag_mutability = "MUTABLE"
-}
-
-# Check if appointment-service repository exists
-resource "null_resource" "check_appointment_service_repo" {
-  provisioner "local-exec" {
-    command = "aws ecr describe-repositories --repository-names ${var.appointment_service_repo_name} || true"
-  }
-}
-
-# Create appointment-service repository if it doesn't exist
-resource "aws_ecr_repository" "appointment_service" {
-  depends_on = [null_resource.check_appointment_service_repo]
-  name       = var.appointment_service_repo_name
-  image_tag_mutability = "MUTABLE"
+data "aws_ecr_repository" "appointment_service" {
+  name = var.appointment_service_repo_name
 }
