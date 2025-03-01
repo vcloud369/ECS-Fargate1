b98afc460a85732687c9c08270cf7090127e9a49
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-03-02 00:18:28 +0530

    Update main.tf

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,7 +1,9 @@
+# Reference the existing patient-service repository
 data "aws_ecr_repository" "patient_service" {
   name = var.patient_service_repo_name
 }
 
+# Reference the existing appointment-service repository
 data "aws_ecr_repository" "appointment_service" {
   name = var.appointment_service_repo_name
 }
