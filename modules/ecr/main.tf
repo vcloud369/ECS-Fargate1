5b616dfd1072389d36ab96a00953043acbec8290
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-03-01 18:32:51 +0530

    Update main.tf

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,19 +1,7 @@
-data "aws_ecr_repository" "patient_service" {
-  repository_name = "patient-service"
-}
-
 resource "aws_ecr_repository" "patient_service" {
-  count             = length(data.aws_ecr_repository.patient_service) == 0 ? 1 : 0
-  repository_name   = "patient-service"
-  image_tag_mutability = "MUTABLE"
-}
-
-data "aws_ecr_repository" "appointment_service" {
-  repository_name = "appointment-service"
+  name = var.patient_service_repo_name
 }
 
 resource "aws_ecr_repository" "appointment_service" {
-  count             = length(data.aws_ecr_repository.patient_service) == 0 ? 1 : 0
-  repository_name   = "appointment-service"
-  image_tag_mutability = "MUTABLE"
+  name = var.appointment_service_repo_name
 }
